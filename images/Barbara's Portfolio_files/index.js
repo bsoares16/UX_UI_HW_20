cb0fa51731e2c5952d8c0600846f4034b610ba99
bsoares16 <70732971+bsoares16@users.noreply.github.com>
2021-02-13 01:22:23 -0500

    Began html set up of page

---
+++ images/Barbara's Portfolio_files/index.js
@@ -0,0 +1,9 @@
+console.log("You index.js file is loaded correctly!");
+function myFunction() {
+  var x = document.getElementById("myTopnav");
+  if (x.className === "topnav") {
+    x.className += " responsive";
+  } else {
+    x.className = "topnav";
+  }
+}
